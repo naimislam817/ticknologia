acbc23a95b963781cc0f34669746ebdfb77866b4
naimislam817 <naimislam231@gmail.com>
2025-10-25 17:51:15 +0600

    added product homepage details

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import picture from '../assets/producthomebanner.png';
+import './ProductPage.css';
+import { FaSearch } from "react-icons/fa";
+
 function ProductPage() {
   return (
     <div>
-    <div><img src="/assets/1.jpeg" alt="Product" />
-</div>   
+   <div className='productbanner'>
+      <img src={picture} alt="" />
+    </div> 
+     <div className='producthomesearch'><input type="search" name="Search " placeholder='Search Product here'/>
+     <button className='searchbuttonproduct'><FaSearch /></button>
+     <div className='filtercategory'>
+     <label htmlFor="Category">  Category  </label> 
+      <select name="Category" id="CategorySelect">
+      <option value="Male">Male</option>
+      <option value="Female">Female</option>
+      <option value="All">All</option>
+    </select>
     </div>
+  </div>
+    <div>
+   </div>  
+   </div>   
   )
 }
 
